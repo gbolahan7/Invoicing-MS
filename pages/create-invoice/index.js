0e9d2419832403b34d2db1f4e5259bade331150b
Abass <abass_gbolahan@yahoo.com>
2022-10-20 13:12:25 +0100

    adding & deletion of items function created

--- pages/create-invoice/index.js
+++ pages/create-invoice/index.js
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
+import { useRouter } from 'next/router';
 
 const CreateInvoice = () => {
+
+    const router = useRouter();
+    const [items, setItems] = useState([]);
+
+    // add product
+
+    const addItem = ()=> {
+        setItems([...items, {name:'', quantity:0, price:0, total:0}])
+        // console.log(items);
+    }
+
+    // handling of the item/product input added
+    const handlerChange = (event, i)=>{
+        const {name, value} = event.target;
+        const list = [...items]
+        list[i][name] = value;
+        list[i]['total'] = list[i]['quantity'] * list[i]['price'];
+        setItems(list)
+    };
+
+    //delete item/product
+    const deleteItem =(i)=>{
+        const inputData = [...items];
+        inputData.splice(i, 1)
+        setItems(inputData)
+    }
   return (
     <div className="main__container">
         <div className="new__invoice">
@@ -31,11 +58,105 @@ const CreateInvoice = () => {
                         </div>
                     </div>
                 </div>
+            
+                <div className="invoice__to">
+                    <p className="invoice__heading"> Invoice to</p>
+                    <div className="form__group">
+                        <p>Customer Name</p>
+                        <input type="text"></input>
+                    </div>
+                    <div className="form__group">
+                        <p>Customer Email</p>
+                        <input type="email"></input>
+                    </div>
 
+                    <div className="form__group">
+                        <p>Street Address</p>
+                        <input type="text"></input>
+                    </div>
+                    <div className="form__group inline__form-group">
+                        <div>
+                            <p>City</p>
+                            <input type="text"/>
+                        </div>
+
+                        <div>
+                            <p>Postal Code</p>
+                            <input type="text"/>
+                        </div>
+
+                        <div>
+                            <p>Country</p>
+                            <input type="text"/>
+                        </div>
+                    </div>
+
+                    <div className="form__group inline__form-group">
+                        <div className="inline__group">
+                            <p>Invoice Date</p>
+                            <input type="date"/>
+                        </div>
+
+                        <div className="inline__group">
+                            <p>Payment Terms</p>
+                            <input type="text"/>
+                        </div>
+                    </div>
+
+                    <div className="form__group">
+                        <p>Descriptiom</p>
+                        <input type="text"></input>
+                    </div>
+                </div>
+
+
+                <div className="invoice__items">
+                    <h3>Product List</h3>
+                    {
+                        items?.map((item,i)=>(
+                            <div className="item" key={i}>
+
+                        <div className="form__group inline__form-group">
+                            <div>
+                                <p>Product Name</p>
+                                <input type="text" name="name" onChange={e=>handlerChange(e,i)}/>
+                            </div>
+
+                            <div>
+                                <p>Qty</p>
+                                <input type="number" name="quantity" onChange={e=>handlerChange(e,i)}/>
+                            </div>
+
+                            <div>
+                                <p>Price</p>
+                                <input type="number" name="price" onChange={e=>handlerChange(e,i)}/>
+                            </div>
+
+                            <div>
+                                <p>Total</p>
+                                <h4>{item.total}</h4>
+                            </div>
+                            <div>
+                            <button className="edit__btn" onClick={()=>deleteItem()}>Delete</button> 
+                            </div> 
+                    </div>
+                </div>  
+                        ))
+                    }                 
+            </div>
+                <button className="add__item-btn" onClick={addItem}>Add New Item</button>
+                <div className="new__invoice-btn">
+                    <button className="edit__btn" onClick={ () => router.push('/')}>Discard</button>
+                    <div>
+                        <button className="draft__btn">Save as draft</button>
+                        <button className="mark__as-btn">Send & Save</button>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     </div>
-  )
-}
+  );
+};
 
 export default CreateInvoice
